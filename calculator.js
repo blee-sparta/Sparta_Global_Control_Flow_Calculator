b3c0338aaa9e820e7175a74680a4be08c80cfc9a
Jason Blee <jblee@spartaglobal.com>
2018-10-01 17:50:53 +0100

    first commit with a working calculator with a loop

---
+++ calculator.js
@@ -0,0 +1,34 @@
+
+while (value1 != "x") {
+
+    // get the user inputs
+    var value1 = prompt('enter first value or Type (x) to quit');
+    if (value1 == "x") {
+      break;
+    }
+    var value2 = prompt('enter the second value');
+    var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide') || "a";
+
+    switch (choice) {
+      case "a":
+        var result = parseInt(value1) + parseInt(value2)
+        alert(`your answer is ${result}`);
+        break;
+      case "s":
+        var result = value1-value2
+        alert(`your answer is ${result}`)
+        break;
+      case "m":
+        var result = value1*value2
+        alert(`your answer is ${result}`)
+        break;
+      case "d":
+        var result = value1/value2
+        alert(`your answer is ${result}`)
+        break;
+      default:
+        var result = parseInt(value1) + parseInt(value2)
+        alert(`your answer is ${result}`)
+        break;
+    }
+    }
